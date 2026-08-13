80c55481643c8e0389023f8214d8cd94bbc385ce
JosueEliRodriguezTec <josueeli.rodriguez@tec.mx>
2026-08-13 17:32:49 -0600

    Update trivia.js

--- trivia.js
+++ trivia.js
@@ -351,7 +351,7 @@ document.addEventListener("touchend", function(e){
 
 
 // =========================================
-// PRUEBA BOTÓN ATRÁS
+// PRUEBA DEL BOTÓN ATRÁS
 // =========================================
 
 history.pushState(null, "", location.href);
